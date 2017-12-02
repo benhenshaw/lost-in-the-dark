284f08f861c27c883e17651c3e5a71e812e9bb66
Charlie Ringer <cring002@gold.ac.uk>
2017-12-02 14:36:55 +0000

    Now the first arg to the net script is the port you want to listen on. The second is the ip address and port you want to send to.

--- net.sh
+++ net.sh
@@ -1,4 +1,4 @@
-sudo socat udp-listen:420 stdout | ./game | socat stdin udp-sendto:10.100.23.169:421
+sudo socat udp-listen:$1 stdout | ./game | socat stdin udp-sendto:$2
 
 # sudo socat udp-listen:420 stdout
 # socat stdin udp-sendto:10.100.23.169:421
