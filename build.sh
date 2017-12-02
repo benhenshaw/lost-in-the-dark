362522851ea144694f0a008961f632d8220161a8
Benedict Henshaw <benedicthenshaw@gmail.com>
2017-12-02 13:57:00 +0000

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
-# FLAGS="djinn.c -o djinn -O2"
-# FLAGS="second_prototype.c -o djinn -O2 -Wall"
-FLAGS="djinnet.c -o djinn -O2 -Wall"
+FLAGS="game.c -o game -O2 -Wall"
 
 clang $FLAGS -framework SDL2
 # gcc $FLAGS -mwindow -lmingw32 -lSDL2main -lSDL2
 
 # if [[ $? -eq 0 ]]; then
-#     ./djinn
-#     rm djinn
+#     ./game
+#     rm game
 # fi
